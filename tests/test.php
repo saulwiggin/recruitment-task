5440f8a88acc2610ece8de5101b67ce875af6cad
saulwiggin <saulwiggin@googlemail.com>
2017-11-05 14:23:09 +0000

    tests

--- tests/test.php
+++ tests/test.php
@@ -1,26 +1,27 @@
 <?php
 
-//namespace Users\user\dev\thirdBridge\recruitmentTask\tests\
+namespace \Users\usr\dev\third-bridge\recruitment-task*\CmdLineLoader
 
-class Test extends \PHPUnit_Framework_TestCase
+use PHPUnit/Framework/TestCase;
+
+class Test extends TestCase
 {
     // test is a class
     public function testisclass(){
         $foo = true;
-        $this->assertTrue($foo)
+        $this->assertTrue($foo);
     }
 
     // test one optional input
     public function testOneArgument(){
         $arg1 = 'data/file.csv';
-        $this->expectOutputString('print to command line number of active values: 900');
+        $this->expectOutputString($this->CmdLineLoader($arg1),'print to command line number of active values: 900');
 
     }
     //test second optional input
     public function testSecondArgument(){
-        $arg1 = "--input='data/file.csv'";
-        $arg2 = "--output='results/answer.txt";
-        $this->expectOutputString('print answer to results/answer.txt');
+        $arg2 = "--input='data/file.csv' --output='results/answer.txt";
+        $this->expectOutputString($this->CmdLineLoader($arg2)'print answer to results/answer.txt');
     }
 }
 
